d7dcebbe3c62b14176f11ab3ea77eabde689203f
bp-Toyota <bp_toyoda@minacare.co.jp>
2021-04-15 14:19:56 +0900

    プロパティ保持解除

--- ageDeterminationApps/ViewController/TopScreen/TopScreenViewController.swift
+++ ageDeterminationApps/ViewController/TopScreen/TopScreenViewController.swift
@@ -66,10 +66,9 @@ class TopScreenViewController: UIViewController, UIImagePickerControllerDelegate
     
     @IBAction func launchCamera(sender: UIButton) {
         // カメラ起動
-        let camera = UIImagePickerController.SourceType.camera
-        if UIImagePickerController.isSourceTypeAvailable(camera) {
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerController.SourceType.camera) {
             let picker = UIImagePickerController()
-            picker.sourceType = camera
+            picker.sourceType = UIImagePickerController.SourceType.camera
             picker.delegate = self
             self.present(picker, animated: true)
         }
