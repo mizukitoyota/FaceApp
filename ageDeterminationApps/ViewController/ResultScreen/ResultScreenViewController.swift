1a9495629256976dd0ae3cd08780b57c688a8248
mizukitoyota <mtoyota@system-renovate.co.jp>
2021-05-06 17:45:49 +0900

    顔画像結果実装

---
+++ ageDeterminationApps/ViewController/ResultScreen/ResultScreenViewController.swift
@@ -0,0 +1,9 @@
+//
+//  ResultScreenViewController.swift
+//  ageDeterminationApps
+//
+//  Created by Training on 2021/05/06.
+//  Copyright © 2021 training. All rights reserved.
+//
+
+import Foundation
--- ageDeterminationApps/ViewModel/TopScreen/TopScreenViewModel.swift
+++ ageDeterminationApps/ViewModel/TopScreen/TopScreenViewModel.swift
@@ -7,10 +7,12 @@
 //
 
 import UIKit
+import Alamofire
 
 class TopScreenViewModel {
-
-    /// ModelとViewの仲介
-    /// Viewからユーザアクションの通知を受けModelのデータを更新
-    /// Modelからデータの更新通知を受けViewの表示を更
+    func getArticles() {
+        // APIへリクエストを送信
+        //Alamofire.Request
+    }
+    
 }
