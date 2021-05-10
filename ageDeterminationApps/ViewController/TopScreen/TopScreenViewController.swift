a466f56d94629de45ae1c329e0656cfac18868d1
mizukitoyota <mtoyota@system-renovate.co.jp>
2021-05-10 22:33:40 +0900

    API通信仮実装

--- ageDeterminationApps/ViewController/TopScreen/TopScreenViewController.swift
+++ ageDeterminationApps/ViewController/TopScreen/TopScreenViewController.swift
@@ -7,8 +7,12 @@
 //
 
 import UIKit
+//HTTPリクエスト
+import Alamofire
+//（ユーザー情報の保存）JSONファイル解析
+import SwiftyJSON
 
-class TopScreenViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class TopScreenViewController: UIViewController, UIImagePickerControllerDelegate,URLSessionDelegate, URLSessionDataDelegate, UINavigationControllerDelegate {
     @IBOutlet var topScreenView: UIView!
     @IBOutlet weak var topTitleNavigationBar: UINavigationBar!
     @IBOutlet weak var topButtonView: UIView!
@@ -84,6 +88,7 @@ class TopScreenViewController: UIViewController, UIImagePickerControllerDelegate
     
     @IBAction func submitButton(_ sender: Any) {
         // 画面遷移、API
+        // self.getImages()
         // 画面遷移
         let storyboard: UIStoryboard = UIStoryboard(name: "ResultScreen", bundle: nil)
         // StoryboardIDを指定してViewControllerを取得する
@@ -96,4 +101,48 @@ class TopScreenViewController: UIViewController, UIImagePickerControllerDelegate
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
         self.dismiss(animated: true)
     }
-}
+    
+    
+    // 検索キーワードの値を元に画像を引っ張ってくる
+    // pixabay.com
+    func getImages(){
+        
+//        let photo = topImageView.image
+//        let imageData = photo!.pngData()!as NSData
+//
+//        let base64String = imageData.base64EncodedString(options: .lineLength64Characters)
+//        let imageString = base64String
+//
+//        // APIKEY: 2FeR2N3--DLdJOV8RmYi7snd4oplNrpy
+//        // SecretKey: 0vp2s3Ihe22KEYa_TiWHbLqbbr857EN_
+//        // API URL: https://api-us.faceplusplus.com/facepp/v3/face/analyze
+//        let url = "https://api-us.faceplusplus.com/facepp/v3/face/analyze"
+//        // \(imageString)
+//        // responseJSONーAPIサーバーとの通信処理
+//        // Alamofireを使ってhttpリクエストを投げます。getでリクエストを投げる（URL形式）、responseが返ってくる
+//        AF.request(url, method: .get, parameters: nil, encoding: JSONEncoding.default).responseJSON { (response) in
+//
+//            // キャスト
+//            let json:JSON = JSON(response.data as Any)
+        // 通信用のConfigを生成.
+        // let configer = ["api_key":API_KEY,"api_secret":API_SECRET,"image_base64":img_file,"return_landmark":1,"return_attributes":"gender,age,smiling,headpose,facequality,blur,eyestatus,emotion,ethnicity,beauty,mouthstatus,eyegaze,skinstatus"]
+        let config:URLSessionConfiguration = URLSessionConfiguration.background(withIdentifier: "login")
+        // Sessionを生成.
+        let session: URLSession = URLSession(configuration: config, delegate: self, delegateQueue: nil)
+        // 通信先のURLを生成.
+        let myUrl:URL = URL(string: "https://api-us.faceplusplus.com/facepp/v3/face/analyze")!
+        // POST用のリクエストを生成.
+        var myRequest:URLRequest = URLRequest(url: myUrl)
+        myRequest.httpMethod = "POST"
+        // 送信するデータを生成、リクエストにセット.
+        let str: NSString = "api_key=\("2FeR2N3--DLdJOV8RmYi7snd4oplNrpy")&api_secret=\("0vp2s3Ihe22KEYa_TiWHbLqbbr857EN_")" as NSString
+        let myData: NSData = str.data(using: String.Encoding.utf8.rawValue)! as NSData
+        myRequest.httpBody = myData as Data
+         
+        // タスクの生成.
+        let task: URLSessionDataTask = session.dataTask(with: myRequest as URLRequest)
+        // タスクの実行.
+        task.resume()
+        }
+    }
+
