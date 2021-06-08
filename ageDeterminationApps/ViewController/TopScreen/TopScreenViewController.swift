6d6933532197b7edca3eaccc7d06e812e4b1bc2e
mizukitoyota <mtoyota@system-renovate.co.jp>
2021-06-08 19:01:28 +0900

    API実装

--- ageDeterminationApps/ViewController/TopScreen/TopScreenViewController.swift
+++ ageDeterminationApps/ViewController/TopScreen/TopScreenViewController.swift
@@ -20,8 +20,17 @@ struct Test {
     public var age: Int?
     public var beauty_woman: Int?
     public var beauty_man: Int?
+    public var emotion_surprise: Int?
+    public var emotion_neutral: Int?
+    public var emotion_anger: Int?
+    public var emotion_disgust: Int?
+    public var emotion_happiness: Int?
+    public var emotion_sadness: Int?
+    public var emotion_fear: Int?
+//init(image_id: String, request_id: String, time_used: Int, gender: String, age: Int, beauty_woman: Int, beauty_man: Int, emotion_surprise: Int, emotion_neutral: Int, emotion_anger: Int, emotion_disgust: Int, emotion_happiness: Int, emotion_sadness: Int, emotion_fear: Int)
+//    self.
 }
-
+    
 class TopScreenViewController: UIViewController, UIImagePickerControllerDelegate,URLSessionDelegate, URLSessionDataDelegate, UINavigationControllerDelegate {
     @IBOutlet var topScreenView: UIView!
     @IBOutlet weak var topTitleNavigationBar: UINavigationBar!
@@ -31,11 +40,11 @@ class TopScreenViewController: UIViewController, UIImagePickerControllerDelegate
     @IBOutlet weak var cameraButton: UIButton!
     @IBOutlet weak var faceApiButton: UIButton!
     @IBOutlet weak var topNavigationTitleItem: UINavigationItem!
-    
     var coupons: [[String: Any]] = [] { //パースした[String: Any]型のクーポンデータを格納するメンバ変数
         didSet{
         }
     }
+    let viewModel = TopScreenViewModel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -103,9 +112,10 @@ class TopScreenViewController: UIViewController, UIImagePickerControllerDelegate
     
     @IBAction func submitButton(_ sender: Any) {
         // 画面遷移、API
-        self.getImages(onSuccess: {_ in},
+        getImages(onSuccess: {_ in},
                        onError: {_ in})
-        self.viewmodel
+//        viewmodels(onSuccess: {_ in},
+//                             onError: {_ in})
         // 画面遷移
         let storyboard: UIStoryboard = UIStoryboard(name: "ResultScreen", bundle: nil)
         // StoryboardIDを指定してViewControllerを取得する
@@ -118,14 +128,12 @@ class TopScreenViewController: UIViewController, UIImagePickerControllerDelegate
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
         self.dismiss(animated: true)
     }
-    
-    
     // 検索キーワードの値を元に画像を引っ張ってくる
     // pixabay.com
     func getImages(onSuccess: @escaping(Result<JSON, Error>) -> Void,
                    onError: @escaping(Error) -> Void) {
         
-        let photo = topImageView.image
+        let photo = self.topImageView.image
         let imageData = photo!.jpegData(compressionQuality: 0.2)
         
         guard let base64String = imageData?.base64EncodedString() else {
@@ -136,9 +144,9 @@ class TopScreenViewController: UIViewController, UIImagePickerControllerDelegate
         // API URL: https://api-us.faceplusplus.com/facepp/v3/detect
         // キャスト
         //            let json:JSON = JSON(response.data as Any)
-        let config:URLSessionConfiguration = URLSessionConfiguration.default
+        // let config:URLSessionConfiguration = URLSessionConfiguration.default
         // Sessionを生成.
-        let session: URLSession = URLSession(configuration: config, delegate: self, delegateQueue: nil)
+        // let session: URLSession = URLSession(configuration: config, delegate: self, delegateQueue: nil)
         // 通信先のURLを生成.
         //        let myUrl:URL = URL(string: "https://api-us.faceplusplus.com/facepp/v3/detect")!
         // POST用のリクエストを生成.
@@ -170,33 +178,39 @@ class TopScreenViewController: UIViewController, UIImagePickerControllerDelegate
         }
     }
     
-    func viewmodel(onSuccess: (Result<JSON, Error>) -> Void,
-                   onError: (Error) -> Void) {
-        self.getImages(
-            onSuccess: { result in
-                switch result {
-                case .success(let value):
-                    let attributes = value["faces"][0]["attributes"]
-                    let gender = attributes["gender"]["value"].string
-                    let beauty_woman = attributes["beauty"]["female_score"].int
-                    let beauty_man = attributes["beauty"]["male_score"].int
-                    let age = attributes["age"]["value"].int
-                    
-                    let emotion_surprise = attributes["emotion"]["surprise"].int
-                    let emotion_neutral = attributes["emotion"]["neutral"].int
-                    let emotion_anger = attributes["emotion"]["anger"].int
-                    let emotion_disgust = attributes["emotion"]["disgust"].int
-                    let emotion_happiness = attributes["emotion"]["happiness"].int
-                    let emotion_sadness = attributes["emotion"]["sadness"].int
-                    let emotion_fear = attributes["emotion"]["fear"].int
-                    
-                    print(gender ?? "No_Gender",beauty_woman ?? "No_WomanLady",beauty_man ?? "No_NiceGay",age ?? "No_Age")
-                case .failure(let error):
-                    print(error)
-                }
-            },
-            onError: { _ in
-                
-            })
+//    func viewmodels(onSuccess: (Result<JSON, Error>) -> Void,
+//                    onError: (Error) -> Void) {
+//        getImages(
+//            onSuccess: { result in
+//                switch result {
+//                case .success(let value):
+//                    let attributes = value["faces"][0]["attributes"]
+//                    let gender = attributes["gender"]["value"].string
+//                    let beauty_woman = attributes["beauty"]["female_score"].int
+//                    let beauty_man = attributes["beauty"]["male_score"].int
+//                    let age = attributes["age"]["value"].int
+//
+//                    let emotion_surprise = attributes["emotion"]["surprise"].int
+//                    let emotion_neutral = attributes["emotion"]["neutral"].int
+//                    let emotion_anger = attributes["emotion"]["anger"].int
+//                    let emotion_disgust = attributes["emotion"]["disgust"].int
+//                    let emotion_happiness = attributes["emotion"]["happiness"].int
+//                    let emotion_sadness = attributes["emotion"]["sadness"].int
+//                    let emotion_fear = attributes["emotion"]["fear"].int
+//
+//                    let datas = Test( gender: gender, age: age, beauty_woman: beauty_woman, beauty_man: beauty_man, emotion_surprise: emotion_surprise, emotion_neutral: emotion_neutral, emotion_anger: emotion_anger, emotion_disgust: emotion_disgust, emotion_happiness: emotion_happiness, emotion_sadness: emotion_sadness, emotion_fear: emotion_fear)
+//                    func aaa(){
+//                        print(datas)
+//                    }
+//                    print(gender ?? "No_Gender",beauty_woman ?? "No_WomanLady",beauty_man ?? "No_NiceGay",age ?? "No_Age")
+//                case .failure(let error):
+//                    print(error)
+//                }
+//            },
+//            onError: { _ in
+//            })
+//    }
+    func ast(){
+        getImages(onSuccess: {_ in}, onError: {_ in})
     }
 }
--- ageDeterminationApps/ViewModel/TopScreen/TopScreenViewModel.swift
+++ ageDeterminationApps/ViewModel/TopScreen/TopScreenViewModel.swift
@@ -7,12 +7,41 @@
 //
 
 import UIKit
+//HTTPリクエスト
 import Alamofire
+//（ユーザー情報の保存）JSONファイル解析
+import SwiftyJSON
 
-class TopScreenViewModel {
-    func getArticles() {
-        // APIへリクエストを送信
-        //Alamofire.Request
+class TopScreenViewModel{
+    var viewController = TopScreenViewController()
+    func viewmodels(onSuccess: (Result<JSON, Error>) -> Void,
+                    onError: (Error) -> Void) {
+        viewController.getImages(
+            onSuccess: { result in
+                switch result {
+                case .success(let value):
+                    let attributes = value["faces"][0]["attributes"]
+                    let gender = attributes["gender"]["value"].string
+                    let beauty_woman = attributes["beauty"]["female_score"].int
+                    let beauty_man = attributes["beauty"]["male_score"].int
+                    let age = attributes["age"]["value"].int
+                    
+                    let emotion_surprise = attributes["emotion"]["surprise"].int
+                    let emotion_neutral = attributes["emotion"]["neutral"].int
+                    let emotion_anger = attributes["emotion"]["anger"].int
+                    let emotion_disgust = attributes["emotion"]["disgust"].int
+                    let emotion_happiness = attributes["emotion"]["happiness"].int
+                    let emotion_sadness = attributes["emotion"]["sadness"].int
+                    let emotion_fear = attributes["emotion"]["fear"].int
+                    
+                    let datas = Test( gender: gender, age: age, beauty_woman: beauty_woman, beauty_man: beauty_man, emotion_surprise: emotion_surprise, emotion_neutral: emotion_neutral, emotion_anger: emotion_anger, emotion_disgust: emotion_disgust, emotion_happiness: emotion_happiness, emotion_sadness: emotion_sadness, emotion_fear: emotion_fear)
+            
+                    print(gender ?? "No_Gender",beauty_woman ?? "No_WomanLady",beauty_man ?? "No_NiceGay",age ?? "No_Age")
+                case .failure(let error):
+                    print(error)
+                }
+            },
+            onError: { _ in
+            })
     }
-    
 }
